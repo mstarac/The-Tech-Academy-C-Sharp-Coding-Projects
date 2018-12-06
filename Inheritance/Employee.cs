50c7c2c01d135ce18e9176720ab8925500da3db7
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-05 21:34:50 -0800

    Inheritance Changes

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Employee : Person
+    {
+        public new void SayName()
+
+        {
+            int ID = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}

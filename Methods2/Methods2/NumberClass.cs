ad0b4f20e584c21e0ef7e47bbe3d66ac9b1a00c9
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-05 21:07:12 -0800

    Methods 2 fixes

---
+++ Methods2/Methods2/NumberClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+     public class NumberClass
+    {
+        public void NumberModifier(int x, int y)
+        {
+            x = x + 5;
+            Console.WriteLine(y);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int two = Convert.ToInt32(Console.ReadLine());
+
+            NumberClass result = new NumberClass();
+            result.NumberModifier(one, two);
+            result.NumberModifier(5, 4);
+
+            Console.ReadLine();
+        }
+    }
+}

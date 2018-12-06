340c3015030e1661aa7bcc3e6c7fee61fe97af50
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-05 20:23:54 -0800

    Methods1 update

---
+++ Methods1/Methods1/MathOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOne
+{
+
+    public class OneMath
+    {
+        public OneMath(int a)
+        {
+            this.a = a * 2;
+        }
+        public int a { get; set; }
+    }
+    public class TwoMath
+    {
+        public TwoMath(int b)
+        {
+            this.b = b / 2;
+        }
+        public int b { get; set; }
+    }
+    public class ThreeMath
+    {
+        public ThreeMath(int c)
+        {
+            this.c = c * c;
+        }
+        public int c { get; set; }
+    }
+}
+
+
---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number.");
+            int value = Convert.ToInt32(Console.ReadLine());
+            OneMath outcome1 = new OneMath(value);
+            Console.WriteLine(outcome1.a);
+
+
+            Console.WriteLine("Please enter a number.");
+            TwoMath outcome2 = new TwoMath(value);
+            Console.WriteLine(outcome2.b);
+
+
+            Console.WriteLine("Please enter a number.");
+            ThreeMath outcome3 = new ThreeMath(value);
+            Console.WriteLine(outcome3.c);
+
+
+            Console.ReadLine();
+        }
+    }
+}

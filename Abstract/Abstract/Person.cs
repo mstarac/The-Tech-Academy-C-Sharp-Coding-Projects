e634d9f5e0344cc6e1a5e19e423d617a0628acbf
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-08 19:09:57 -0800

    Abstract Drill

---
+++ Abstract/Abstract/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+       
+
+        }
+    }
+
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public class Program
+    {
+        public  static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}

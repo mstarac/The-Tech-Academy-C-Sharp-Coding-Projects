072822a92d190691c640acf71554ac06d8c93195
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-10 12:25:40 -0800

    Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 25;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}

f9950fa50d86679df1e190c0d52442bd5f76c482
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-09 23:33:40 -0800

    Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum DaysOfTheWeek
+
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}       
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+            string dayEntered = Console.ReadLine();
+
+
+            try
+            {
+                Enum.Parse(typeof(DayOfWeek), dayEntered);
+                Console.WriteLine(" Today is " + dayEntered);
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+         }
+      }
+   }
---
+++ Enums/ExceptionHandlingDrill106/ExceptionHandlingDrill106/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+    {
+        static void Main()
+        {
+        try
+        {
+
+            List<int> intList = new List<int>();
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+            intList.Add(50);
+            intList.Add(60);
+            intList.Add(70);
+            intList.Add(80);
+
+            Console.WriteLine("Please enter a number to divide list numbers by. ");
+            int numberEntered = Convert.ToInt32(Console.ReadLine());
+            foreach (int i in intList)
+            {
+                Console.WriteLine(i / numberEntered);
+            }
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please enter an integer.");
+        }
+
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please do not divide by zero.");
+        }
+        finally
+        {
+
+            Console.ReadLine();
+        }
+    }
+       
+ }
+
+
+
+
+
+//        }
+//        finally
+//        {
+
+//            Console.WriteLine("Please enter a number to divide list numbers by.");
+//            int numberEntered = Convert.ToInt32(Console.ReadLine());
+//        }
+

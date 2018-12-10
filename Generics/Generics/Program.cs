ad3c32f045d17266daed226b9aea5534d97174c7
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-09 20:11:30 -0800

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeList = new Employee<string>();
+            Employee<int> employeeInt = new Employee<int>();
+            employeeList.things = new List<string>() { "Sally", "Smith" };
+            employeeInt.things = new List<int>();
+            employeeInt.things.Add(10);
+            employeeInt.things.Add(20);
+
+            foreach (string item in employeeList.things)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int item in employeeInt.things)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+

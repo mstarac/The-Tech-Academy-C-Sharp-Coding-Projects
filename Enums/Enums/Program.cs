544b1114bb34973f20c91273c88bca6dfee91a6d
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-12 15:22:32 -0800

    Enums updated

--- Enums/Enums/Program.cs
+++ Enums/Enums/Program.cs
@@ -22,19 +22,30 @@ namespace Enums
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter the current day of the week.");
-            string dayEntered = Console.ReadLine();
-
+            
 
             try
             {
+                string dayEntered = Console.ReadLine();
+                dayEntered = dayEntered.ToLower();
+                string firstLetter = dayEntered.ToUpper().Substring(0, 1);
+                dayEntered = firstLetter + dayEntered.Substring(1);
+
+
                 Enum.Parse(typeof(DayOfWeek), dayEntered);
                 Console.WriteLine(" Today is " + dayEntered);
                 Console.ReadLine();
+
+                
             }
             catch (FormatException)
             {
                 Console.WriteLine("Please enter an actual day of the week.");
                 Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Error, please enter a valid day of the week");
             }
          }
       }

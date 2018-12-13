b3a444db695bee61fe52cbc6b26439c9ff8805b3
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-13 12:39:27 -0800

    DateTimeDrill

---
+++ DateTimeDrill/DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        
+                
+        public static void Main()
+        { 
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The date and time is currently" +" " + " " + now);
+           
+            Console.WriteLine("Please enter a number between 1 and 12.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours it will be {1}", x, now.AddHours(x));
+            Console.ReadLine();
+        }
+    }
+}

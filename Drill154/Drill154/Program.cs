cc7eb44690ab161a4f4bb12005f384afd890ee5d
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-12 22:11:07 -0800

    Drill154

---
+++ Drill154/Drill154/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Drill154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Here is some text.";
+            File.WriteAllText(@"C:\Users\Student\Desktop\FileIO.txt", text);
+            Console.WriteLine("Here is some text.");
+            Console.ReadLine();
+
+        }
+    }
+}

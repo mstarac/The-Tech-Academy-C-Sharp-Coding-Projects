4845a872982d0670f460ddf5b4b1685b2b5a6737
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-14 17:03:05 -0800

    Drill 162

---
+++ Drill162/Drill162/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill162
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string santasHouse = "The North Pole";
+
+            var santasAge = "How old is Santa";
+
+            Console.WriteLine(santasHouse, santasAge);
+            Console.ReadLine();
+            }
+        }
+    }
+    
---
+++ Drill162/Drill162/Reindeer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill162
+{
+    public class Reindeer
+    {
+        public string House { get; set; }
+        public int age { get; set; }
+
+        public Reindeer(string House) : this(House, 500)
+        {
+            return;
+        }
+        public Reindeer(string house, int age)
+        {
+            House = house;
+            age = 250;
+        }
+
+       
+    }
+}    
+               
+

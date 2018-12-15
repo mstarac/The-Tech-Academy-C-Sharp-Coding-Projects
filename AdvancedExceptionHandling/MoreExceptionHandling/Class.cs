258f50a730277e77d64509dcc2b764940060ae10
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-14 20:02:34 -0800

    Advanced Exception Handling

---
+++ AdvancedExceptionHandling/MoreExceptionHandling/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExceptionHandling
+{
+    public class Class : Exception
+    {
+        public Class() : base() { }
+
+        public Class(string message) : base(message) { }
+    }
+}
---
+++ AdvancedExceptionHandling/MoreExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validAnswer = false;
+            int age = 0;
+            try
+            {
+                while (!validAnswer && age <= 0)
+                {
+
+                    Console.WriteLine("Please enter your age.");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+
+                    if (!validAnswer) Console.WriteLine("Please enter whole numbers only.");
+                    {
+                        if (age < 0)
+                            throw new InvalidOperationException();
+                    }
+                    Console.WriteLine("You have entered:" + age + "years old.");
+                    Console.ReadLine();
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Error, you are not following directions.");
+                Console.ReadLine();
+
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Please enter your age in whole digits.");
+                Console.ReadLine();
+            }
+        
+
+            }
+        }
+    }
+
